98e0ae268fe507af37831106d55a1a6cdc1c8704
fschweikhardt <schweikhardt.web@gmail.com>
2021-03-29 22:26:23 -0400

    no errors

--- src/config.js
+++ src/config.js
@@ -3,5 +3,4 @@ export default {
     API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
     API_TOKEN: process.env.REACT_APP_API_TOKEN,
     IMG_KEY: process.env.REACT_APP_IMG_KEY 
-    //|| '563492ad6f91700001000001cda85fe2c69f49e99526ea5ef4fd1ace',
 }
